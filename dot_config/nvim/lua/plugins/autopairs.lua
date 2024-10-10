7ed8cc6d2606f9e5d0d7c9c18c583aeb3ba9e42c
Lukas Schröder <lukasscr@proton.me>
2024-10-10 12:02:25 +0200

    Fix neovim auto-closing pairs not working

--- dot_config/nvim/lua/plugins/autopairs.lua
+++ dot_config/nvim/lua/plugins/autopairs.lua
@@ -1,19 +1,19 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
-	config = true,
 	dependencies = {
 		"hrsh7th/nvim-cmp",
 	},
-	opts = {
-		check_ts = true,
-		ts_config = {
-			lua = { "string" },
-		},
-	},
 	config = function()
+		local pairs = require("nvim-autopairs")
+		pairs.setup({
+			check_ts = true,
+			ts_config = {
+				lua = { "string" },
+			},
+		})
 		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 		local cmp = require("cmp")
 		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-	end
+	end,
 }
