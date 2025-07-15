25f8d9b3f1a9a72093ba565349abc67f04acdbb8
Lukas Schröder <lukasscr@proton.me>
2025-07-15 16:36:35 +0200

    Fix linting on linux

--- dot_config/nvim/lua/plugins/linting.lua
+++ dot_config/nvim/lua/plugins/linting.lua
@@ -8,7 +8,7 @@ local function is_installed(exec)
 	if vim.fn.executable(exec) == 1 then
 		return true
 	else
-		print("Program '" .. exec .. "' is not available via mason, please install manually!")
+		vim.notify("Program '" .. exec .. "' is not available via mason, please install manually!", vim.log.levels.WARN)
 		return false
 	end
 end
@@ -16,15 +16,17 @@ end
 local function get_linters_by_ft()
 	-- Make sure the first element in the table is the linters name
 	local linters = {
-		bash = { "bash" },
-		c = { "clangtidy" },
-		cpp = { "clangtidy" },
+		bash = { "shellcheck" },
 		haskell = { "hlint" },
 		lua = { "luacheck" },
 		python = { "ruff" },
 	}
 
-	if is_installed("clippy") then
+	if is_installed("clang-tidy") then
+		linters["c"] = { "clangtidy" } -- not available via mason, needs to be installed manually
+		linters["cpp"] = { "clangtidy" } -- not available via mason, needs to be installed manually
+	end
+	if is_installed("clippy-driver") then
 		linters["rust"] = { "clippy" } -- not available via mason, needs to be installed manually
 	end
 	if get_uname() == "x86_64-Linux" then
@@ -41,10 +43,9 @@ _G.tools = _G.tools or {}
 _G.tools.linters = function()
 	-- Because basically none of the linter names match the mason registry packages...
 	return {
-		"clangd", -- for clangtidy
 		"luacheck",
 		"ruff",
-		"shellcheck", -- for bash
+		"shellcheck",
 		"hlint",
 	}
 end
@@ -58,6 +59,8 @@ return {
 	config = function()
 		local lint = require("lint")
 		lint.linters_by_ft = get_linters_by_ft()
+
+		lint.linters.clippy.ignore_exitcode = true
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 		vim.api.nvim_create_autocmd({
