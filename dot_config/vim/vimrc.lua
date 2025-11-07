90c2a2a0c565e29bac331ca5fd0983d0879546f9
Lukas Schröder <lukasscr@proton.me>
2025-11-07 02:50:14 +0100

    Format source code on save
    I've added nvim autocommands to the vimrc file which will format lua,
    python and rust code once a corresponding source file is written and the
    formatting tool is installed on the system.

--- dot_config/vim/vimrc.lua
+++ dot_config/vim/vimrc.lua
@@ -25,3 +25,38 @@ vim.opt.spell = true
 vim.cmd("syntax enable")
 vim.cmd("filetype plugin indent on")
 
+-- Auto-format specific filetypes on save if the formatters are available
+vim.api.nvim_create_augroup("AutoFormat", {})
+
+-- Format Lua files
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = "*.lua",
+	group = "AutoFormat",
+	callback = function()
+		if vim.fn.executable("stylua") == 1 then
+			vim.cmd("silent !stylua %")
+		end
+	end,
+})
+
+-- Format Python files
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = "*.py",
+	group = "AutoFormat",
+	callback = function()
+		if vim.fn.executable("ruff") == 1 then
+			vim.cmd("silent !ruff format -s %")
+		end
+	end,
+})
+
+-- Format Rust files
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = "*.rs",
+	group = "AutoFormat",
+	callback = function()
+		if vim.fn.executable("rustfmt") == 1 then
+			vim.cmd("silent !rustfmt -q %")
+		end
+	end,
+})
