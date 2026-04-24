f8cb0b17770f05676dc165aee147666ae7e26b0e
Lukas Schröder <lukasscr@proton.me>
2026-04-25 00:04:45 +0200

    Enable treesitter-based highlighting and folds

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,13 +1,21 @@
 vim.pack.add({
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", name = "nvim-treesitter" },
+	{
+		src = "https://github.com/nvim-treesitter/nvim-treesitter",
+		name = "nvim-treesitter",
+		version = "main",
+	},
 })
 
-require("nvim-treesitter").install({
+local ts = require("nvim-treesitter")
+
+-- Install the following parsers
+ts.install({
 	"bash",
 	"c",
 	"cpp",
 	"css",
 	"cuda",
+	"commonlisp",
 	"hcl",
 	"html",
 	"lua",
@@ -17,4 +25,25 @@ require("nvim-treesitter").install({
 	"yaml",
 	"rust",
 	"zig",
+})
+
+vim.o.foldlevelstart = 99
+
+-- Register parser-specific functionality for corresponding file types
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function(args)
+		-- Use treesitter-based highlighting
+		local ok = pcall(vim.treesitter.start, args.buf)
+		if not ok then
+			return  -- short-circuit if no parser is available
+		end
+
+		-- Enable treesitter-based folds
+		vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+		vim.wo.foldmethod = "expr"
+		vim.o.foldlevel = 99
+
+		-- Base indent on treesitter (experimental)
+		vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+	end,
 })
